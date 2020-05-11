49af2fd87c1c2aff9cafdb2a5b10be49610e4878
fartem <jaman.smlnsk@gmail.com>
2020-05-11 10:24:52 +0300

    2020-05-11 Update: Completed "Dicee" app

--- 4_dicee/lib/main.dart
+++ 4_dicee/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
   MaterialApp(
@@ -15,35 +16,65 @@ void main() => runApp(
   )
 );
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  int _leftDiceNumber = 1;
+  int _rightDiceNumber = 1;
+
+  @override
+  void initState() {
+    _controller = AnimationController(vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    var leftDiceNumber = 1;
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-            child: FlatButton(
-              onPressed: () {
-
-              },
-              child: Image.asset(
-                  'images/dice$leftDiceNumber.png'
-              ),
-            )
+              child: FlatButton(
+                onPressed: () {
+                  setState(() => _dicee());
+                },
+                child: Image.asset(
+                    'images/dice$_leftDiceNumber.png'
+                ),
+              )
           ),
           Expanded(
             child: FlatButton(
               onPressed: () {
-
+                setState(() => _dicee());
               },
               child: Image.asset(
-                  'images/dice2.png'
+                  'images/dice$_rightDiceNumber.png'
               ),
             ),
           )
         ],
       ),
     );
+  }
+
+  void _dicee() {
+    _leftDiceNumber = _randomDiceNumber();
+    _rightDiceNumber = _randomDiceNumber();
+  }
+
+  int _randomDiceNumber() {
+    return Random().nextInt(6) + 1;
   }
 }
