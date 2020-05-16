6f686bd34fecd818a65830c5d1086213df859166
fartem <jaman.smlnsk@gmail.com>
2020-05-16 15:51:56 +0300

    2020-05-16 Update: Updated "Xylophone" app

--- 6_xylophone/lib/main.dart
+++ 6_xylophone/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
 
@@ -6,58 +7,45 @@ void main() => runApp(
 );
 
 class XylophoneApp extends StatelessWidget {
+  final _keyColors = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.teal,
+    Colors.blue,
+    Colors.purple
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              FlatButton(
-                color: Colors.red,
-                onPressed: () {
-                  _playSound(1);
-                },
-              ),
-              FlatButton(
-                color: Colors.orange,
-                onPressed: () {
-                  _playSound(2);
-                },
-              ),
-              FlatButton(
-                color: Colors.yellow,
-                onPressed: () {
-                  _playSound(3);
-                },
-              ),
-              FlatButton(
-                color: Colors.green,
-                onPressed: () {
-                  _playSound(4);
-                },
-              ),
-              FlatButton(
-                color: Colors.teal,
-                onPressed: () {
-                  _playSound(5);
-                },
-              ),
-              FlatButton(
-                color: Colors.blue,
-                onPressed: () {
-                  _playSound(6);
-                },
-              ),
-              FlatButton(
-                color: Colors.purple,
-                onPressed: () {
-                  _playSound(7);
-                },
-              )
-            ],
-          ),
+        backgroundColor: Colors.black,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            _buildKey(1),
+            _buildKey(2),
+            _buildKey(3),
+            _buildKey(4),
+            _buildKey(5),
+            _buildKey(6),
+            _buildKey(7)
+          ],
         ),
+      ),
+    );
+  }
+  
+  Expanded _buildKey(int note) {
+    return Expanded(
+      // ignore: missing_required_param
+      child: FlatButton(
+        color: _keyColors[note - 1],
+        onPressed: () {
+          _playSound(note);
+        },
       ),
     );
   }
